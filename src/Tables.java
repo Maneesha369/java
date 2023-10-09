6ff6eb54434ad8550db795c27c3fe1426cef5445
Maneesha Gujjula <maneeshagujjula123@gmail.com>
2023-10-09 23:33:44 +0530

    second commit

---
+++ src/Tables.java
@@ -0,0 +1,12 @@
+public class Tables {
+        public static void main(String[] args) {
+            int number = 7;
+
+            System.out.println("Multiplication table of " + number + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(number + " x " + i + " = " + (number * i));
+            }
+        }
+}
+
+
