2b8deff9701875a6d3b679c3bb89144ee8a5be68
Maneesha Gujjula <maneeshagujjula123@gmail.com>
2023-10-09 23:04:15 +0530

    second commit

---
+++ src/FactorialWithIoop.java
@@ -0,0 +1,17 @@
+public class FactorialWithIoop {
+
+        public static void main(String[] args) {
+            int num = 5;
+            int factorial = 1;
+            int i = 1;
+
+            while (i <= num) {
+                factorial *= i;
+                i++;
+            }
+
+            System.out.println("Factorial of " + num + " is: " + factorial);
+        }
+}
+
+
