e97856e1d3823751bfe4e8797926b57d24b57281
Maneesha Gujjula <maneeshagujjula123@gmail.com>
2023-10-09 22:57:24 +0530

    first commit

---
+++ src/Even_Or_Odd.java
@@ -0,0 +1,14 @@
+public class Even_Or_Odd {
+    public static void even(int n) {
+        if (n% 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        even(20);
+
+    }
+}
